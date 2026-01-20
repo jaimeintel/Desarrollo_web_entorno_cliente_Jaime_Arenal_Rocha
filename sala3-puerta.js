47d460fd4378d78af2ffd47ee03418ea41689d4a
Jaime Arenal Rocha <jaimearenalrocha@gmail.com>
2026-01-20 09:42:49 +0100

    Cambios en las propiedades a privada
    Para mayor seguridad, he cambiado totalPuertasAbiertas a privada, para que no se pueda modificar fácilmente.

--- sala3-puerta.js
+++ sala3-puerta.js
@@ -1,6 +1,6 @@
 export default class Puerta {
-  static totalPuertasAbiertas = 0;
-  #estado = "Cerrada";
+  static #totalPuertasAbiertas = 0; // info global (privada) que afecta a todas las puetas 
+  #estado = "Cerrada"; // propiedad privada, si se hace Puerta.#estado dará error. 
 
   constructor(material, color, tipoContraseña) {
     this.material = material;
@@ -11,15 +11,15 @@ export default class Puerta {
   abrir() {
     if (this.estado === "Cerrada") {
       this.#estado = "Abierta";
-      Puerta.totalPuertasAbiertas++;
+      Puerta.#totalPuertasAbiertas++; // se suma +1 puertas abiertas
     }
   }
 
-  get estado() {
+  get estado() { // se crea metodo para saber estado 
     return this.#estado;
   }
 
-  static mostrarNumeroPuertasAbiertas() {
-    return Puerta.totalPuertasAbiertas;
+  static mostrarNumeroPuertasAbiertas() { // metodo estatico para saber total de puertas abiertas. 
+    return Puerta.#totalPuertasAbiertas;
   }
 }
