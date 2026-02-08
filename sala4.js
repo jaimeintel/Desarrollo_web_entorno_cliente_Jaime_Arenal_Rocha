b630155e0cf7dcdc6b1c4b693c76320a9c3f30ab
Jaime Arenal Rocha <jaimearenalrocha@gmail.com>
2026-02-08 18:28:30 +0100

    nuevo addeventlistener mouseenter
    Cambia la imagen del cursor y se arreglan condiciones para que funcione el juego cuando por fin clicas en el centro del cuadrado.

--- sala4.js
+++ sala4.js
@@ -1,28 +1,36 @@
 const caja = document.getElementById("caja");
 const texto = document.getElementById("datos");
 
-// mousemove para ver la posicion del raton y mostramos por pantalla. 
+// ancho y alto de caja 
+let anchoCaja = caja.offsetWidth;
+let altoCaja = caja.offsetHeight;
+
+// mousemove para ver la posicion del raton y mostramos por pantalla.
 caja.addEventListener("mousemove", function (e) {
   let x = e.offsetX;
   let y = e.offsetY;
 
+  if (x == anchoCaja / 2 && y == altoCaja / 2){
+    caja.style.backgroundColor = "green";
+  }
+
   texto.innerText = `Coordenadas del ratón en la caja: X=${x}, Y=${y}`;
+});
+
+caja.addEventListener("mouseenter", () => {
+  caja.style.cursor = "url('images/objetivo2.png'), auto";
 });
 
 // click para poder ver donde se hizo click y si coincide con el centro
 caja.addEventListener("click", function (e) {
   // para saber ancho y alto de la caja
-  let anchoCaja = caja.offsetWidth;
-  let altoCaja = caja.offsetHeight;
 
   let clickX = e.offsetX;
   let clickY = e.offsetY;
 
-  if ((clickX = anchoCaja / 2) && (clickY = altoCaja / 2)) {
-    caja.style.backgroundColor = "red";
-    alert(
-      `Has pulsado justo en centro de la caja !! felicidades ! `,
-    );
+  if (clickX == anchoCaja / 2 && clickY == altoCaja / 2) {
+    caja.style.backgroundColor = "green";
+    alert(`Has pulsado justo en centro de la caja !! felicidades ! `);
   } else {
     caja.style.backgroundColor = "red";
     alert(`NO has pulsado en el centro justo, vuelve a intentarlo... `);
